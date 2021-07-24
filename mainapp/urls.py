27f6db3e5d7e6074eb87288229bc188fd83518c8
mongus <kjyanus@gmail.com>
2021-07-24 11:38:28 +0900

    templates create

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -3,4 +3,9 @@ from .views import *
 
 urlpatterns = [
     path("", home, name="home"),
+    path("ott/", home, name="ott"),
+    path("music/", home, name="music"),
+    path("shopping/", home, name="shopping"),
+    path("iservice/", home, name="iservice"),
+    path("delievery/", home, name="delievery"),
 ]

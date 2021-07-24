017317dc2b9edb83fe3c4b7e0c5281ec3b6cae52
mongus <kjyanus@gmail.com>
2021-07-24 12:55:03 +0900

    login

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path("ott/", home, name="ott"),
     path("music/", home, name="music"),
     path("shopping/", home, name="shopping"),
-    path("iservice/", home, name="iservice"),
-    path("delievery/", home, name="delievery"),
+    path("etc/", home, name="etc"),
+
 ]

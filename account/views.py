e2ec121ce5bf70630606397c14cb4a590e1dbb45
myway00 <junior0614@naver.com>
2021-07-24 11:38:55 +0900

    init

--- account/views.py
+++ account/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from . import views
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import authenticate, login, logout 
 
-# Create your views here.
+def login_view(request):
+    form=AuthenticationForm
+    return render(request, 'login.html',{'form' : form})

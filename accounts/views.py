429bd56d71b729ec3f034b84e27e6c6373e0d7a8
myway00 <junior0614@naver.com>
2021-07-24 11:01:23 +0900

    login

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+
+def login_view(request):
+    form=AuthenticationForm
+    return render(request, 'login.html',{'form' : form})
+
+
 
 # Create your views here.
--- payMonth/settings.py
+++ payMonth/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mainapp',
-    'settings',
+    'accounts',
 ]
 
 MIDDLEWARE = [
